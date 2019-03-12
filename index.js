4eefa753397c96cfd27196d534b766a5454ea195
Tien Nguyen <nghuuanhtien@gmail.com>
2019-03-12 08:58:01 +0700

    Copy code

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 $(document).ready( function() {
 
-  // function setCookie(cname, cvalue, exdays) {
-  //   var d = new Date();
-  //   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  //   var expires = "expires="+d.toUTCString();
-  //   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  // }
-
   function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
@@ -17,52 +10,15 @@ $(document).ready( function() {
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
   }
 
-  function getStorage(cname) {
-    let obj = JSON.parse(localStorage.getItem(cname));
-    console.log("get", obj);
-    let result = null
-    const now = Date.now();
-    if (obj) {
-      var timestamp = obj.timestamp;
-      console.log(timestamp)
-      console.log(now)
-      console.log(timestamp > now)
-      if (timestamp > now) {
-        return obj.value;
-      }
-    }
-    return result
-  }
+  let code = getParameterByName("code");
+  $("#code").val(code);
 
-  function checkStorage() {
 
-    org_id = getStorage("org_id")
-    user_id = getStorage("user_id")
-    tenant_id = getStorage("tenant_id")
-    agent_id = getStorage("agent_id")
+  let button = document.getElementById('copyCode');
 
-    if (org_id && tenant_id && user_id && agent_id) {
-      $.ajax({
-        type: "POST",
-        url: "https://zohovcs.requestcatcher.com/register",
-        data: {
-          tenant_id: tenant_id,
-          org_id: org_id
-        },
-        success: function(){
-        },
-        dataType: dataType
-      });
-
-      $.post("https://zohovcs.requestcatcher.com/register", {tenant_id: tenant_id, org_id: org_id}).then(function(){
-        alert("OkAy")
-      })
-    } else {
-      alert("yOu sTupiD, set storage first ~~")
-    }
-  }
-
-  // checkStorage();
-  console.log(getParameterByName("code"));
+  button.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.execCommand('copy', false, document.getElementById('code').select());
+  });
 
 });
